539c12b1f4cf8278ddbc4d3ee827691bbc2243ec
ichisemasashi <ichise.masashi@gmail.com>
2018-03-21 21:36:08 +0900

    空白文字

---
+++ 01_simple-parser.hs
@@ -0,0 +1,17 @@
+import Text.ParserCombinators.Parsec hiding (spaces)
+import System.Environment
+
+spaces :: Parser ()
+spaces = skipMany1 space
+
+symbol :: Parser Char
+symbol = oneOf "!#$%&|*+-/:<=>?@_~"
+
+readExpr :: String -> String
+readExpr input = case parse (spaces >> symbol) "lisp" input of
+  Left err -> "No match: " ++ show err
+  Right val -> "Found value"
+
+main :: IO ()
+main = do args <- getArgs
+          putStrLn (readExpr (args !! 0))
